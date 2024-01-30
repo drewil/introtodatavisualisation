7967db9f6d72134b9a8b22a5d72b6416669cadc1
Dhruvil <shahd883@newschool.edu>
2024-01-29 19:08:04 -0500

    Error fixed

---
+++ app.js
@@ -0,0 +1,7 @@
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
+
+console.log(d3);
+
+const body = d3.select('body');
+
+const img = body.append('img').attr("src" , https://unsplash.com/photos/a-person-standing-on-top-of-a-sand-dune-gVDUuJaG_wM);
